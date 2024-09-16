fe82440b3ff7ee34067d2465e926d752059542f5
Natalia Iacono <167995391+NataliaIacono@users.noreply.github.com>
2024-09-16 09:36:27 +0000

    tablas y relaciones ok

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -7,6 +8,31 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    relation_favoritos = relationship ("Favoritos"); backref="user"
+
+
+class Personajes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    lightsaber_user = db.Column(db.Boolean(),unique=False, nullable=False)
+
+    relation_favoritos = relationship ("Favoritos"); backref="personajes"
+
+class Planetas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+    relation_favoritos = relationship ("Favoritos"); backref="planetas"
+
+class Favoritos(db.Model):
+    id = db.Column(db.Integer, ForeignKey("user.id"), primary_key=True)
+    planeta_favorito = db.Column(db.Integer, ForeignKey('planetas.name'))
+    personaje_favorito = db.Column(db.Integer, ForeignKey('personajes.name'))
+
+    def to_dict(self):
+        return {}
+
 
     def __repr__(self):
         return '<User %r>' % self.username
